f4b6bd72a65ff28ec59a913cb4a99ac790c57da8
Dharaneesh0745 <dharaneesh0745@gmail.com>
2023-12-31 10:16:37 +0530

    Solved errors.

--- components/router.js
+++ components/router.js
@@ -1,8 +1,7 @@
-import Expertise from "./Expertise"
-import Banner from "./Banner"
-import Testimonial from "./Testimonial"
-import ShowCase from "./ShowCase"
-import Brand from "./Brand"
-import BlogCard from "./BlogCard"
+import Expertise from "./Expertise";
+import Banner from "./Banner";
+import Testimonial from "./Testimonial";
+import ShowCase from "./ShowCase";
+import Brand from "./Brand";
 
-export { Expertise, Banner, Testimonial, ShowCase, Brand, BlogCard }
+export { Expertise, Banner, Testimonial, ShowCase, Brand };
