bee440323df536c2da6232b16ba8ff4ca9fc6bd0
Dharaneesh0745 <dharaneesh0745@gmail.com>
2023-12-31 08:23:29 +0530

    Header modified

--- components/common/Header.js
+++ components/common/Header.js
@@ -1,56 +1,93 @@
-import Link from "next/link"
-import { TitleLogo } from "./Title"
-import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
-import { RiMenu4Line } from "react-icons/ri"
-import { AiOutlineClose } from "react-icons/ai"
+import Link from "next/link";
+import { TitleLogo } from "./Title";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { RiMenu4Line } from "react-icons/ri";
+import { AiOutlineClose } from "react-icons/ai";
+import Image from "next/image";
 
 const Header = () => {
-  const [activeLink, setActiveLink] = useState("")
-  const [open, setOpen] = useState(false)
+  const [activeLink, setActiveLink] = useState("");
+  const [open, setOpen] = useState(false);
 
-  const router = useRouter()
+  const router = useRouter();
   useEffect(() => {
-    setActiveLink(router.pathname)
-  }, [router.pathname])
+    setActiveLink(router.pathname);
+  }, [router.pathname]);
   return (
     <>
       <header>
-        <div className='container'>
-          <div className='logo'>
-            <Link href='/'>
-              <TitleLogo title='creative' caption='7' className='logomin' />
+        <div className="container">
+          <div className="logo">
+            <Link href="/">
+              <TitleLogo
+                title=" with Doctors"
+                caption="Learn"
+                className="logomin"
+              />
+              <Image
+                className="doctor"
+                src={"/doctor.png"}
+                width="30"
+                height="30"
+              ></Image>
             </Link>
           </div>
-          <nav className={open ? "openMenu" : "closeMenu"} onClick={() => setOpen(null)}>
-            <Link href='/' className={activeLink == "/" ? "activeLink" : "none"}>
+          <nav
+            className={open ? "openMenu" : "closeMenu"}
+            onClick={() => setOpen(null)}
+          >
+            <Link
+              href="/"
+              className={activeLink == "/" ? "activeLink" : "none"}
+            >
               Home
             </Link>
-            <Link href='/agency' className={activeLink == "/agency" ? "activeLink" : "none"}>
-              Agency
+            <Link
+              href="/agency"
+              className={activeLink == "/agency" ? "activeLink" : "none"}
+            >
+              Our Team
             </Link>
-            <Link href='/team' className={activeLink == "/team" ? "activeLink" : "none"}>
+            {/* <Link
+              href="/team"
+              className={activeLink == "/team" ? "activeLink" : "none"}
+            >
               Team
-            </Link>
-            <Link href='/services' className={activeLink == "/services" ? "activeLink" : "none"}>
+            </Link> */}
+            {/* <Link
+              href="/services"
+              className={activeLink == "/services" ? "activeLink" : "none"}
+            >
               Services
             </Link>
-            <Link href='/showcase' className={activeLink == "/showcase" ? "activeLink" : "none"}>
+            <Link
+              href="/showcase"
+              className={activeLink == "/showcase" ? "activeLink" : "none"}
+            >
               Showcase
-            </Link>
-            <Link href='/blogs' className={activeLink == "/blogs" ? "activeLink" : "none"}>
+            </Link> */}
+            <Link
+              href="/blogs"
+              className={activeLink == "/blogs" ? "activeLink" : "none"}
+            >
               Blog
             </Link>
-            <Link href='/contact' className={activeLink == "/contact" ? "activeLink" : "none"}>
+            <Link
+              href="/contact"
+              className={activeLink == "/contact" ? "activeLink" : "none"}
+            >
               Contact
             </Link>
-            <button className='button-primary'>book a consultation</button>
+            <button className="button-primary">Get Started</button>
           </nav>
-          <button onClick={() => setOpen(!open)}>{open ? <AiOutlineClose size={25} /> : <RiMenu4Line size={25} />}</button>
+          <button onClick={() => setOpen(!open)}>
+            {open ? <AiOutlineClose size={25} /> : <RiMenu4Line size={25} />}
+          </button>
         </div>
       </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
